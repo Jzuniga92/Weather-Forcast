758acd1a72176b2130a864f77d6ad394f5def01c
Joselyn Zuniga <joselynzuniga@joselyns-mbp.attlocal.net>
2023-03-07 16:26:39 -0600

    updated html and js file

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,14 @@
 // API Key - Weather Update
 var WeatherUpdate = "2a317f77bfd417765ed57cf7c23b14b9";
 var city;
+var btnSearch = document.querySelector('#search')
+var searchCity = document.querySelector('#city-search')
 
+btnSearch.addEventListener("click",function() {
+    console.log (searchCity.value)
+})
 
-
-
+  //function getForecast = ""
+  //API call
+  // Search city pull current forecast
+  
